6a4488a1e6a600d29914a2fc1849d6ae31947697
Franklin Byaruhanga <byaruhaf@appbantu.com>
2022-11-28 16:22:08 +0300

    start of personal site

---
+++ Content/posts/first-post.md
@@ -0,0 +1,8 @@
+---
+date: 2022-11-28 15:28
+description: A description of my first post.
+tags: first, article
+---
+# My first post
+
+My first post's text.
---
+++ Sources/ByaruhafDev/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct ByaruhafDev: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://byaruhaf.dev")!
+    var name = "Franklin Byaruhanga"
+    var description = "A description of ByaruhafDev"
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try ByaruhafDev().publish(withTheme: .foundation)
