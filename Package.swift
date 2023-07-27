bf44f17fe1306f3d2ae1d686d81df8d7a3f1c6bb
Franklin Byaruhanga <byaruhaf@protonmail.com>
2023-07-27 03:57:27 +0300

    fixing publish warnings

--- Package.swift
+++ Package.swift
@@ -12,21 +12,24 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin.git", from: "0.2.0"),
+        .package(url: "https://github.com/johnsundell/splashpublishplugin.git", from: "0.2.0"),
         .package(url: "https://github.com/insidegui/TwitterPublishPlugin.git", from: "0.2.0"),
         .package(url: "https://github.com/tanabe1478/YoutubePublishPlugin.git", from: "1.0.1"),
         .package(url: "https://github.com/alexito4/ReadingTimePublishPlugin", from: "0.3.0"),
         .package(url: "https://github.com/yonomitt/VimeoPublishPlugin.git", from: "0.2.1"),
         .package(url: "https://github.com/thomaslupo/GistPublishPlugin.git", from: "0.1.0"),
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.9.0")
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.9.0")
     ],
     targets: [
         .executableTarget(
             name: "ByaruhafDev",
-            dependencies: ["Publish","SplashPublishPlugin","TwitterPublishPlugin","YoutubePublishPlugin","VimeoPublishPlugin","GistPublishPlugin", "ReadingTimePublishPlugin"]
+            dependencies: [.product(name: "Publish", package: "publish"),
+                           .product(name: "SplashPublishPlugin", package: "splashpublishplugin"),
+                           "TwitterPublishPlugin",
+                           "YoutubePublishPlugin",
+                           "VimeoPublishPlugin",
+                           "GistPublishPlugin",
+                           "ReadingTimePublishPlugin"]
         )
     ]
 )
-
-//
-//
